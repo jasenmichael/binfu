026f255f125463b573a3920483b7fc3f57218561
jasenmichael <jasen@jasenmichael.com>
2025-03-31 10:07:03 -0500

    test: add tests

---
+++ test/index.test.ts
@@ -0,0 +1,9 @@
+import { describe, expect, it } from "vitest";
+
+import {} from "../playground/template/src";
+
+describe("packageName", () => {
+  it.todo("pass", () => {
+    expect(true).toBe(true);
+  });
+});
